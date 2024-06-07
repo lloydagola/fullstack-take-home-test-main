44f273bc192a7ed928f53b12ebf8e2fc32d9dceb
lloydagola <lloydagola@users.noreply.github.com>
2024-06-07 18:09:59 +0300

    refactoring

--- frontend/src/layouts/_tests_/AppLayout.test.tsx
+++ frontend/src/layouts/_tests_/AppLayout.test.tsx
@@ -5,7 +5,7 @@ import AppLayout from "../AppLayout";
 const elementText = "Testing AppLayout";
 
 describe("testing component rendering", () => {
-  test("test component rendering", () => {
+  test("test that component is rendered in the DOM", () => {
     render(
       <AppLayout>
         <div>{elementText}</div>
@@ -16,6 +16,17 @@ describe("testing component rendering", () => {
     const childElement = screen.getByText(elementText);
 
     expect(appLayout).toBeInTheDocument();
+  });
+  test("test that component renders children", () => {
+    render(
+      <AppLayout>
+        <div>{elementText}</div>
+      </AppLayout>
+    );
+
+    const appLayout = screen.getByRole("main");
+    const childElement = screen.getByText(elementText);
+
     expect(appLayout).toContainElement(childElement);
   });
 });
