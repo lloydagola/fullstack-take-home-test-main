0a1d4600497daa12c7848487f55538236ce39f44
lloydagola <lloydagola@users.noreply.github.com>
2024-06-08 15:15:10 +0300

    configured absolute path

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -28,6 +28,7 @@ module.exports = {
       components: path.resolve(__dirname, "src/components/"),
       views: path.resolve(__dirname, "src/views/"),
       layouts: path.resolve(__dirname, "src/layouts/"),
+      _mocks_: path.resolve(__dirname, "src/_mocks_/"),
     },
   },
   mode: "development",
