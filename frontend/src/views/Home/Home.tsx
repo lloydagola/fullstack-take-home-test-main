e91837c4081c46de571a427a01710200f642d85a
lloydagola <lloydagola@users.noreply.github.com>
2024-06-08 15:20:43 +0300

    rendered books in home page

--- frontend/src/views/Home/Home.tsx
+++ frontend/src/views/Home/Home.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import Grid from "@mui/material/Grid";
+import Book from "components/Book/Book";
+import { TBook } from "utils/types";
+import { booksData } from "_mocks_/books";
 
-export default function Home(): JSX.Element {
-  return <div>home</div>;
+export default function Books(): JSX.Element {
+  return (
+    <Grid
+      container
+      gap={2}
+      display="grid"
+      justifyContent="center"
+      gridTemplateColumns="repeat(auto-fill, 300px)"
+    >
+      {booksData.map((book: TBook, index: number) => (
+        <Book book={book} />
+      ))}
+    </Grid>
+  );
 }
