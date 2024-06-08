8a0fad3ffcae22ffd5dfc9822eef4430a574411f
lloydagola <lloydagola@users.noreply.github.com>
2024-06-08 22:44:55 +0300

    handle GraphQL query search

--- backend/src/resolvers/index.ts
+++ backend/src/resolvers/index.ts
@@ -3,5 +3,12 @@ import { booksData } from '../data/books';
 export const resolvers = {
   Query: {
     books: () => booksData,
+    book(parent: unknown, args: { title: string; }, context: unknown, info: unknown){
+      const { title } = args;
+      
+      const response = booksData.find((book) => book.title.toLocaleLowerCase() === title.toLocaleLowerCase());
+
+      return response;
+    }
   },
 };
