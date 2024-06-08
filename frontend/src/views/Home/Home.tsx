3c9181edeb57c344088887623be107f616bc305f
lloydagola <lloydagola@users.noreply.github.com>
2024-06-08 17:56:50 +0300

    consume context data

--- frontend/src/views/Home/Home.tsx
+++ frontend/src/views/Home/Home.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import Grid from "@mui/material/Grid";
 import Book from "components/Book/Book";
+import { AppContext } from "contexts/AppContextProvider";
 import { TBook } from "utils/types";
-import { booksData } from "_mocks_/books";
+import Button from "@mui/material/Button";
 
 export default function Books(): JSX.Element {
+  const value = useContext(AppContext);
   return (
     <Grid
       container
@@ -13,8 +15,19 @@ export default function Books(): JSX.Element {
       justifyContent="center"
       gridTemplateColumns="repeat(auto-fill, 300px)"
     >
-      {booksData.map((book: TBook, index: number) => (
-        <Book book={book} key={index} />
+      {value?.books.map((book: TBook, index: number) => (
+        <Book
+          book={book}
+          key={index}
+          Button={
+            <Button
+              variant="contained"
+              onClick={() => value.addToReadingList(book)}
+            >
+              Add to List
+            </Button>
+          }
+        />
       ))}
     </Grid>
   );
