ff4dc80af813b23923c3190cebc4414e0e3bc448
lloydagola <lloydagola@users.noreply.github.com>
2024-06-08 14:52:26 +0300

    hardcoded grid of Books

--- frontend/src/views/Books/Books.tsx
+++ frontend/src/views/Books/Books.tsx
@@ -1,10 +1,44 @@
 import React from "react";
+import Grid from "@mui/material/Grid";
 import Book from "components/Book/Book";
 
 export default function Books(): JSX.Element {
   return (
-    <div>
-      <Book />
-    </div>
+    <Grid
+      container
+      gap={2}
+      display="grid"
+      justifyContent="center"
+      gridTemplateColumns="repeat(auto-fill, 300px)"
+    >
+      <Book
+        title="Curious Princess and the Enchanted Garden"
+        author="Reese Smith"
+        readingLevel="H"
+        thumbnailUrl="assets/image2.webp"
+        data-testid="card"
+      />
+      <Book
+        title="Clever Monster on the Wonder Island"
+        author="Jordan Jones"
+        readingLevel="I"
+        thumbnailUrl="assets/image10.webp"
+        data-testid="card"
+      />
+      <Book
+        title="Happy Knight and the Magic Spell"
+        author="Quinn Brown"
+        readingLevel="I"
+        thumbnailUrl="assets/image10.webp"
+        data-testid="card"
+      />
+      <Book
+        title="Happy Dragon and the Magic Spell"
+        author="Alex Jones"
+        readingLevel="A"
+        thumbnailUrl="assets/image6.webp"
+        data-testid="card"
+      />
+    </Grid>
   );
 }
