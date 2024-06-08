70e90c7318077ed1b61388609f5e70617c54997a
lloydagola <lloydagola@users.noreply.github.com>
2024-06-08 16:32:04 +0300

    setup Provider and client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import router from "./router/routes";
+
+const client = new ApolloClient({
+  uri: "https://api.spacex.land/graphql/",
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
     <React.StrictMode>
-      <RouterProvider router={router} />
+      <ApolloProvider client={client}>
+        <RouterProvider router={router} />
+      </ApolloProvider>
     </React.StrictMode>
   );
 }
