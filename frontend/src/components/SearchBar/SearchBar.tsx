8bf4f3cedfe0f35c528a9c1445387332b5537270
lloydagola <lloydagola@users.noreply.github.com>
2024-06-10 17:59:30 +0300

    implemented search books

--- frontend/src/components/SearchBar/SearchBar.tsx
+++ frontend/src/components/SearchBar/SearchBar.tsx
@@ -1,7 +1,22 @@
-import * as React from "react";
+import React, {
+  ChangeEvent,
+  ChangeEventHandler,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { useQuery } from "@apollo/client";
 import { styled, alpha } from "@mui/material/styles";
 import InputBase from "@mui/material/InputBase";
 import SearchIcon from "@mui/icons-material/Search";
+import Box from "@mui/material/Box";
+import { AppContext } from "contexts/AppContextProvider";
+import { TBook } from "types/types";
+import useDebounce from "hooks/useDebounce";
+import { SEARCH_BOOKS_QUERY } from "queries/books";
+import Button from "@mui/material/Button";
+import { Typography } from "@mui/material";
+import LoadingScreen from "views/LoadingScreen/LoadingScreen";
 
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
@@ -47,6 +62,56 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 }));
 
 export default function SearchBar(): JSX.Element {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredBooks, setFilteredBooks] = useState<TBook[]>([]);
+  const appData = useContext(AppContext);
+
+  /*
+   const searchTerm = "Curious Princess and the Enchanted Garden";
+   */
+  const { loading, error, data } = useQuery(SEARCH_BOOKS_QUERY, {
+    variables: { title: searchTerm },
+  });
+
+  useEffect(() => {
+    console.log("search term changed...", searchTerm);
+  }, [searchTerm]);
+
+  // DeBounce Function
+  useDebounce(
+    (): void => {
+      if (!data || !data.books) setFilteredBooks([]);
+      setFilteredBooks((currentState: TBook[]) => {
+        if (!data || !data.books) return [];
+
+        console.log(searchTerm);
+
+        return data?.books?.filter((book: TBook) =>
+          book.title
+            .trim()
+            .toLowerCase()
+            .includes(searchTerm.trim().toLowerCase())
+        );
+      });
+    },
+    [data, searchTerm],
+    800
+  );
+
+  function handleSearch({
+    target: { value },
+  }: ChangeEvent<HTMLInputElement>): void {
+    setSearchTerm(value);
+  }
+
+  function handleBlur() {
+    console.log("blurry...");
+    //setFilteredBooks([]);
+  }
+  function handleFocus() {
+    console.log("hocus focus...");
+  }
+
   return (
     <Search>
       <SearchIconWrapper>
@@ -54,8 +119,72 @@ export default function SearchBar(): JSX.Element {
       </SearchIconWrapper>
       <StyledInputBase
         placeholder="Search…"
+        value={searchTerm}
         inputProps={{ "aria-label": "search" }}
+        onChange={handleSearch}
+        onBlur={handleBlur}
+        onFocus={handleFocus}
       />
+      <Box
+        position="absolute"
+        flexDirection="column"
+        display={`${
+          searchTerm.trim() && filteredBooks.length > 0 ? "flex" : "none"
+        }`}
+        p={1}
+        overflow="scroll"
+        height="600px"
+        width="100%"
+        boxShadow="0px 2px 2px -1px rgba(0,0,0,0.2),0px 2px 2px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)"
+        sx={{ backgroundColor: "#fff" }}
+      >
+        {loading ? (
+          <Box
+            width="100%"
+            height="100%"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Typography>Loading...</Typography>
+          </Box>
+        ) : error ? (
+          <Typography>an error ocurred</Typography>
+        ) : (
+          searchTerm.trim() &&
+          filteredBooks.map((book, index) => (
+            <Box
+              p={1}
+              borderBottom="1px solid #999"
+              key={index}
+              display="flex"
+              flexDirection="row"
+              alignItems="center"
+            >
+              <img
+                loading="lazy"
+                src={book.coverPhotoURL}
+                width="60px"
+                style={{ margin: "12px" }}
+              />
+              <Box display="flex" flex="1" flexDirection="column">
+                <Typography>{book.title}</Typography>
+                <Button
+                  sx={{
+                    margin: "4px 0",
+                    borderRadius: "16px",
+                    fontWeight: 600,
+                    width: "130px",
+                  }}
+                  variant="contained"
+                >
+                  Add to List
+                </Button>
+              </Box>
+            </Box>
+          ))
+        )}
+      </Box>
     </Search>
   );
 }
