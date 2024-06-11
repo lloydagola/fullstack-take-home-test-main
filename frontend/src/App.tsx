0ad0ad87f3378970edf00d0c9f67d2d2d69c49ce
lloydagola <lloydagola@users.noreply.github.com>
2024-06-11 23:31:39 +0300

    merged dev

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import React, { useEffect, useState } from "react";
 import { RouterProvider } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import router from "./router/routes";
@@ -8,7 +7,6 @@ import { BOOKS_QUERY } from "queries/books";
 import AppContextProvider from "./contexts/AppContextProvider";
 import { TBook } from "types/types";
 
-export default function App(): JSX.Element {
 export default function App(): JSX.Element {
   const { data, loading, error } = useQuery(BOOKS_QUERY);
   const [BookData, setBookData] = useState<TBook[]>([]);
