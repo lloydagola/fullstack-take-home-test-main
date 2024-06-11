7a1757e7dae92acddbb293c45365b7b5460f96ef
lloydagola <lloydagola@users.noreply.github.com>
2024-06-11 22:31:15 +0300

    updated types

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,10 +5,11 @@ import router from "./router/routes";
 import LoadingScreen from "views/LoadingScreen/LoadingScreen";
 import { BOOKS_QUERY } from "queries/books";
 import AppContextProvider from "./contexts/AppContextProvider";
+import { TBook } from "types/types";
 
 export default function App(): JSX.Element {
   const { data, loading, error } = useQuery(BOOKS_QUERY);
-  const [BookData, setBookData] = useState([]);
+  const [BookData, setBookData] = useState<TBook[]>([]);
 
   useEffect(() => {
     setBookData(data?.books);
