ce54c2c23bff579a5b0eb73ef58d1cb597bf4afc
lloydagola <lloydagola@users.noreply.github.com>
2024-06-12 13:08:01 +0300

    updated EndOfLine Sequence to LF

--- frontend/src/views/ReadingList/ReadingList.tsx
+++ frontend/src/views/ReadingList/ReadingList.tsx
@@ -4,6 +4,8 @@ import Button from "@mui/material/Button";
 import Book from "components/Book/Book";
 import { AppContext } from "contexts/AppContextProvider";
 import { TBook } from "types/types";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Box";
 
 export default function ReadingList(): JSX.Element {
   const value = useContext(AppContext);
@@ -15,21 +17,33 @@ export default function ReadingList(): JSX.Element {
       justifyContent="center"
       gridTemplateColumns="repeat(auto-fill, 300px)"
     >
-      {value?.readingList.map((book: TBook, index: number) => (
-        <Book
-          book={book}
-          key={index}
-          Button={
-            <Button
-              variant="contained"
-              color="error"
-              onClick={() => value.removeFromReadingList(book)}
-            >
-              Remove From List
-            </Button>
-          }
-        />
-      ))}
+      {value?.readingList && value?.readingList.length > 0 ? (
+        value.readingList?.map((book: TBook, index: number) => (
+          <Book
+            book={book}
+            key={index}
+            Button={
+              <Button
+                variant="contained"
+                color="error"
+                onClick={() => value.removeFromReadingList(book)}
+              >
+                Remove From List
+              </Button>
+            }
+          />
+        ))
+      ) : (
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          width="100%"
+          height="100vh"
+        >
+          <Typography>You have no books in your reading list...</Typography>
+        </Box>
+      )}
     </Grid>
   );
 }
