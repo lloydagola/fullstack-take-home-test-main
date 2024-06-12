6bad1cd303b165e8888904de3fc4f9b97d7e488d
lloydagola <lloydagola@users.noreply.github.com>
2024-06-12 12:04:40 +0300

    code cleanup

--- frontend/src/views/Home/Home.tsx
+++ frontend/src/views/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 import Grid from "@mui/material/Grid";
 import Book from "components/Book/Book";
 import { AppContext } from "contexts/AppContextProvider";
